67af1840c4ed15902e6fe04f5d1b84520ecd6bd9
Georg Sieber <it@georg-sieber.de>
2025-10-19 19:34:26 +0200

    implement polling for pending certificates

--- app/src/main/java/systems/sieber/droid_scep/MainActivity.java
+++ app/src/main/java/systems/sieber/droid_scep/MainActivity.java
@@ -4,24 +4,29 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.RestrictionsManager;
+import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.security.KeyChain;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	TextView editTextUrl;
-	TextView editTextCommonName;
-	TextView editTextEnrollmentChallenge;
-	TextView editTextKeystorePassword;
+	EditText editTextUrl;
+	EditText editTextCommonName;
+	EditText editTextEnrollmentChallenge;
+	EditText editTextEnrollmentProfile;
+	EditText editTextKeystorePassword;
+	EditText editTextTransactionId;
 	Spinner spinnerKeyLen;
 	String keystoreAlias;
+
+	SharedPreferences sharedPref;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +36,15 @@ public class MainActivity extends Activity {
 		editTextUrl = findViewById(R.id.editTextScepUrl);
 		editTextCommonName = findViewById(R.id.exitTextCommonName);
 		editTextEnrollmentChallenge = findViewById(R.id.editTextEnrollmentChallenge);
+		editTextEnrollmentProfile = findViewById(R.id.editTextEnrollmentProfile);
 		editTextKeystorePassword = findViewById(R.id.editTextKeystorePassword);
+		editTextTransactionId = findViewById(R.id.exitTextTransactionId);
 		spinnerKeyLen = findViewById(R.id.spinnerKeySize);
+
+		sharedPref = getSharedPreferences("temp-store", Context.MODE_PRIVATE);
+		editTextTransactionId.setText( sharedPref.getString("tid", "") );
+
+		keystoreAlias = getString(R.string.default_keystore_alias);
 
 		applyPolicies();
 	}
@@ -51,8 +63,9 @@ public class MainActivity extends Activity {
 			editTextUrl.setText( appRestrictions.getString("scep-url", getString(R.string.default_server_url)) );
 			editTextCommonName.setText( appRestrictions.getString("subject-dn", getString(R.string.default_subject_dn)) );
 			editTextEnrollmentChallenge.setText( appRestrictions.getString("enrollment-challenge", "1FDE8ED526747EADB0681A952963CDE4") );
+			editTextEnrollmentProfile.setText( appRestrictions.getString("enrollment-profile", getString(R.string.default_enrollment_profile)) );
 			editTextKeystorePassword.setText( appRestrictions.getString("keystore-password", "") );
-			keystoreAlias = appRestrictions.getString("keystore-alias", getString(R.string.default_keystore_alias));
+			keystoreAlias = appRestrictions.getString("keystore-alias", keystoreAlias);
 
 			String defaultRsaKeyLen = String.valueOf( appRestrictions.getInt("rsa-key-length", Integer.parseInt(getString(R.string.default_rsa_len))) );
 			SpinnerAdapter adapter = spinnerKeyLen.getAdapter();
@@ -64,7 +77,7 @@ public class MainActivity extends Activity {
 		}
 	}
 
-	public void onClickEnroll(View view) {
+	public void onClickRequest(View view) {
 		CharSequence sURI = editTextUrl.getText();
 		int isKeyLen = Integer.parseInt(String.valueOf(spinnerKeyLen.getSelectedItem()));
 
@@ -77,6 +90,7 @@ public class MainActivity extends Activity {
 					sURI.toString(),
 					editTextCommonName.getText().toString(),
 					editTextEnrollmentChallenge.getText().toString(),
+					editTextEnrollmentProfile.getText().toString(),
 					isKeyLen,
 					keystoreAlias,
 					editTextKeystorePassword.getText().toString()
@@ -87,12 +101,82 @@ public class MainActivity extends Activity {
 			intent.putExtra(KeyChain.EXTRA_PKCS12, keystore);
 			startActivity(intent);
 
+		} catch(ScepClient.RequestPendingException e) {
+			CommonDialog.show(this, e.getClass().getName(), getString(R.string.request_pending_help), CommonDialog.Icon.WARN, false);
+			editTextTransactionId.setText(e.getTransactionId());
+
+			SharedPreferences.Editor editor = sharedPref.edit();
+			editor.putString("cert", e.getCertPem());
+			editor.putString("key", e.getKeyPem());
+			editor.putString("tid", e.getTransactionId());
+			editor.apply();
+
 		} catch(ScepClient.BadRequestException e) {
 			CommonDialog.show(this, e.getClass().getName(), getString(R.string.bad_request_help), CommonDialog.Icon.ERROR, false);
 			e.printStackTrace();
+
 		} catch(Exception e) {
 			CommonDialog.show(this, e.getClass().getName(), e.getMessage(), CommonDialog.Icon.ERROR, false);
 			e.printStackTrace();
 		}
 	}
+
+	public void onClickPoll(View view) {
+		if(sharedPref.getString("cert", "").isEmpty()
+		|| sharedPref.getString("key", "").isEmpty()
+		|| sharedPref.getString("tid", "").isEmpty()) {
+			CommonDialog.show(this, getString(R.string.no_request_pending), "", CommonDialog.Icon.WARN, false);
+			return;
+		}
+
+		CharSequence sURI = editTextUrl.getText();
+
+		// enable some policies
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy);
+
+		try {
+			byte[] keystore = ScepClient.CertPoll(
+					sURI.toString(),
+					sharedPref.getString("cert", ""),
+					sharedPref.getString("key", ""),
+					editTextCommonName.getText().toString(),
+					sharedPref.getString("tid", ""),
+					keystoreAlias,
+					editTextKeystorePassword.getText().toString()
+			);
+
+			// import into system/user keystore
+			Intent intent = KeyChain.createInstallIntent();
+			intent.putExtra(KeyChain.EXTRA_PKCS12, keystore);
+			startActivity(intent);
+
+			// clear temp data
+			SharedPreferences.Editor editor = sharedPref.edit();
+			editor.remove("cert");
+			editor.remove("key");
+			editor.remove("tid");
+			editor.apply();
+			editTextTransactionId.setText("");
+
+		} catch(ScepClient.RequestPendingException e) {
+			CommonDialog.show(this, e.getClass().getName(), getString(R.string.request_pending_help), CommonDialog.Icon.WARN, false);
+			editTextTransactionId.setText(e.getTransactionId());
+
+			SharedPreferences.Editor editor = sharedPref.edit();
+			editor.putString("cert", e.getCertPem());
+			editor.putString("key", e.getKeyPem());
+			editor.putString("tid", e.getTransactionId());
+			editor.apply();
+
+		} catch(ScepClient.BadRequestException e) {
+			CommonDialog.show(this, e.getClass().getName(), getString(R.string.bad_request_help), CommonDialog.Icon.ERROR, false);
+			e.printStackTrace();
+
+		} catch(Exception e) {
+			CommonDialog.show(this, e.getClass().getName(), e.getMessage(), CommonDialog.Icon.ERROR, false);
+			e.printStackTrace();
+		}
+	}
+
 }
--- app/src/main/java/systems/sieber/droid_scep/ScepClient.java
+++ app/src/main/java/systems/sieber/droid_scep/ScepClient.java
@@ -1,23 +1,30 @@
 package systems.sieber.droid_scep;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigInteger;
 
 import java.net.URL;
 
-import java.security.Key;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
 import java.security.cert.CertStore;
 import java.security.cert.CertStoreException;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
@@ -31,6 +38,7 @@ import org.jscep.client.verification.OptimisticCertificateVerifier;
 
 import org.jscep.transaction.FailInfo;
 import org.jscep.transaction.TransactionException;
+import org.jscep.transaction.TransactionId;
 import org.spongycastle.asn1.DERPrintableString;
 import org.spongycastle.asn1.pkcs.PKCSObjectIdentifiers;
 import org.spongycastle.asn1.x500.X500Name;
@@ -43,18 +51,40 @@ import org.spongycastle.operator.OperatorCreationException;
 import org.spongycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.spongycastle.pkcs.PKCS10CertificationRequestBuilder;
 import org.spongycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
+import org.spongycastle.util.encoders.Base64Encoder;
+
+import javax.security.auth.x500.X500Principal;
 
 public class ScepClient {
 
     public static class BadRequestException extends Exception {}
+    public static class RequestPendingException extends Exception {
+        String mTransactionId;
+        String mCert;
+        String mKey;
+        RequestPendingException(String cert, String key, String transactionId) {
+            mCert = cert;
+            mKey = key;
+            mTransactionId = transactionId;
+        }
+        String getCertPem() {
+            return mCert;
+        }
+        String getKeyPem() {
+            return mKey;
+        }
+        String getTransactionId() {
+            return mTransactionId;
+        }
+    }
 
-    public static byte[] CertReq(String enrollentURL, String entityName, String enrollmentChallenge, int isKeyLen, String keystoreAlias, String keystorePassword)
-            throws BadRequestException, ClientException, TransactionException, CertStoreException, NoSuchAlgorithmException, OperatorCreationException, CertificateException, KeyStoreException, NoSuchProviderException, IOException {
+    public static byte[] CertReq(String enrollmentURL, String entityName, String enrollmentChallenge, String enrollmentProfile, int isKeyLen, String keystoreAlias, String keystorePassword)
+            throws BadRequestException, RequestPendingException, ClientException, TransactionException, CertStoreException, NoSuchAlgorithmException, OperatorCreationException, CertificateException, KeyStoreException, NoSuchProviderException, IOException {
 
         // load SpongyCastle
         java.security.Security.addProvider(new BouncyCastleProvider());
 
-        URL server = new URL(enrollentURL);
+        URL server = new URL(enrollmentURL);
 
         CertificateVerifier verifier = new OptimisticCertificateVerifier();
         Client client = new Client(server, verifier);
@@ -68,11 +98,12 @@ public class ScepClient {
 
         // create a self signed cert to sign the PKCS7 envelope
         JcaX509v3CertificateBuilder v3CertGen = new JcaX509v3CertificateBuilder(
-                entity, BigInteger.valueOf(1), new Date(
-                System.currentTimeMillis()), new Date(
-                System.currentTimeMillis()
-                        + (1000L * 60 * 60 * 24 * 100)), entity,
-                keyPair.getPublic());
+                entity, BigInteger.valueOf(1),
+                new Date(System.currentTimeMillis()),
+                new Date(System.currentTimeMillis() + (1000L * 60 * 60 * 24 * 100)),
+                entity,
+                keyPair.getPublic()
+        );
 
         JcaContentSignerBuilder csb = new JcaContentSignerBuilder("SHA256withRSA");
         ContentSigner cs = csb.build(keyPair.getPrivate());
@@ -88,24 +119,71 @@ public class ScepClient {
         DERPrintableString password = new DERPrintableString(enrollmentChallenge);
         crb.addAttribute(PKCSObjectIdentifiers.pkcs_9_at_challengePassword, password);
 
-        // Send the enrollment request
-        EnrollmentResponse response = client.enrol(cert, keyPair.getPrivate(), crb.build(cs), "NDESCA");
-
-        Certificate certz[] = new Certificate[1];
-        // Automatic enrollment, so this should be issued
+        // send the enrollment request
+        EnrollmentResponse response = client.enrol(cert, keyPair.getPrivate(), crb.build(cs), enrollmentProfile);
         if(!response.isSuccess()) {
-            if(response.getFailInfo() == FailInfo.badRequest)
-                throw new BadRequestException();
-            else
-                throw new ClientException(response.getFailInfo().toString());
+            if(response.isPending()) {
+                throw new RequestPendingException(
+                        cert2pem(cert),
+                        key2pem(keyPair.getPrivate()),
+                        response.getTransactionId().toString()
+                );
+            } else if(response.isFailure()) {
+                if(response.getFailInfo() == FailInfo.badRequest)
+                    throw new BadRequestException();
+                else
+                    throw new ClientException(response.getFailInfo().toString());
+            }
+            throw new ClientException("Response is neither success, pending or failure?!");
         }
 
-        CertStore store = response.getCertStore();
+        return parseCertResponse(response.getCertStore(), keyPair.getPrivate(), keystoreAlias, keystorePassword);
+    }
+
+    public static byte[] CertPoll(String enrollmentURL, String certPem, String keyPem, String entityName, String transactionId, String keystoreAlias, String keystorePassword)
+            throws BadRequestException, RequestPendingException, ClientException, TransactionException, InvalidKeySpecException, NoSuchAlgorithmException, CertificateException, IOException, CertStoreException, KeyStoreException, NoSuchProviderException {
+
+        // load SpongyCastle
+        java.security.Security.addProvider(new BouncyCastleProvider());
+
+        URL server = new URL(enrollmentURL);
+
+        CertificateVerifier verifier = new OptimisticCertificateVerifier();
+        Client client = new Client(server, verifier);
+
+        X500Principal entity = new X500Principal(entityName);
+        X509Certificate cert = pem2cert(certPem);
+        PrivateKey key = pem2key(keyPem);
+
+        TransactionId tId = new TransactionId(transactionId.getBytes());
+        EnrollmentResponse response = client.poll(cert, key, entity, tId);
+
+        if(!response.isSuccess()) {
+            if(response.isPending()) {
+                throw new RequestPendingException(
+                        cert2pem(cert),
+                        key2pem(key),
+                        response.getTransactionId().toString()
+                );
+            } else if(response.isFailure()) {
+                if(response.getFailInfo() == FailInfo.badRequest)
+                    throw new BadRequestException();
+                else
+                    throw new ClientException(response.getFailInfo().toString());
+            }
+            throw new ClientException("Response is neither success, pending or failure?!");
+        }
+
+        return parseCertResponse(response.getCertStore(), key, keystoreAlias, keystorePassword);
+    }
+
+    private static byte[] parseCertResponse(CertStore store, PrivateKey key, String keystoreAlias, String keystorePassword)
+            throws CertStoreException, CertificateException, IOException, NoSuchAlgorithmException, KeyStoreException, NoSuchProviderException {
         Collection<? extends Certificate> certs = store.getCertificates(null);
 
         @SuppressWarnings("unchecked")
         Iterator<Certificate> ir = (Iterator<Certificate>) certs.iterator();
-
+        Certificate certz[] = new Certificate[1];
         int i = 0;
         while(ir.hasNext()) {
             certz[i] = ir.next();
@@ -115,8 +193,7 @@ public class ScepClient {
         KeyStore keyStore = KeyStore.getInstance("PKCS12", "BC");
         keyStore.load(null, null);
 
-        Key k = keyPair.getPrivate();
-        keyStore.setKeyEntry(keystoreAlias, k, keystorePassword.toCharArray(), certz);
+        keyStore.setKeyEntry(keystoreAlias, key, keystorePassword.toCharArray(), certz);
 
         ByteArrayOutputStream bout = new ByteArrayOutputStream();
         keyStore.store(bout, keystorePassword.toCharArray()); // this is the password to open the .p12
@@ -125,6 +202,38 @@ public class ScepClient {
         bout.close();
 
         return keystore;
-
     }
+
+    private static String cert2pem(X509Certificate cert) throws CertificateEncodingException, IOException {
+        Base64Encoder encoder = new Base64Encoder();
+        ByteArrayOutputStream s = new ByteArrayOutputStream();
+        encoder.encode(cert.getEncoded(), 0, cert.getEncoded().length, s);
+        return s.toString();
+        //return "-----BEGIN CERTIFICATE-----\n" + s.toString() + "-----END CERTIFICATE-----\n";
+    }
+    private static String key2pem(PrivateKey key) throws IOException {
+        Base64Encoder encoder = new Base64Encoder();
+        ByteArrayOutputStream s = new ByteArrayOutputStream();
+        encoder.encode(key.getEncoded(), 0, key.getEncoded().length, s);
+        return s.toString();
+        //return "-----BEGIN PRIVATE KEY-----\n" + s.toString() + "-----END PRIVATE KEY-----\n";
+    }
+    private static X509Certificate pem2cert(String pem) throws CertificateException, IOException {
+        Base64Encoder encoder = new Base64Encoder();
+        ByteArrayOutputStream s = new ByteArrayOutputStream();
+        encoder.decode(pem.getBytes(), 0, pem.getBytes().length, s);
+        InputStream is = new ByteArrayInputStream(s.toByteArray());
+        return (X509Certificate) CertificateFactory
+                .getInstance("X509")
+                .generateCertificate(is);
+    }
+    private static PrivateKey pem2key(String pem) throws NoSuchAlgorithmException, InvalidKeySpecException, IOException {
+        Base64Encoder encoder = new Base64Encoder();
+        ByteArrayOutputStream s = new ByteArrayOutputStream();
+        encoder.decode(pem.getBytes(), 0, pem.getBytes().length, s);
+        return (PrivateKey) KeyFactory
+                .getInstance("RSA")
+                .generatePrivate(new PKCS8EncodedKeySpec(s.toByteArray()));
+    }
+
 }
