3afd02e1106d068d35d3d2c4e7582317a380ab1b
Georg Sieber <it@georg-sieber.de>
2025-10-20 17:47:51 +0200

    improve UI: execute SCEP requests in background thread, add confirmation before sending a second request

--- app/src/main/java/systems/sieber/droid_scep/MainActivity.java
+++ app/src/main/java/systems/sieber/droid_scep/MainActivity.java
@@ -1,9 +1,13 @@
 package systems.sieber.droid_scep;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.RestrictionsManager;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -11,6 +15,7 @@ import android.security.KeyChain;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
@@ -20,6 +25,8 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+	Button buttonRequest;
+	Button buttonPoll;
 	EditText editTextUrl;
 	EditText editTextCommonName;
 	EditText editTextEnrollmentChallenge;
@@ -41,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
 		setSupportActionBar(toolbar);
 
 		// find views
+		buttonRequest = findViewById(R.id.buttonRequest);
+		buttonPoll = findViewById(R.id.buttonPoll);
 		editTextUrl = findViewById(R.id.editTextScepUrl);
 		editTextCommonName = findViewById(R.id.exitTextCommonName);
 		editTextEnrollmentChallenge = findViewById(R.id.editTextEnrollmentChallenge);
@@ -101,6 +110,37 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	public void onClickRequest(View view) {
+		if(sharedPref.getString("cert", "").isEmpty()
+				|| sharedPref.getString("key", "").isEmpty()
+				|| sharedPref.getString("tid", "").isEmpty()) {
+			request();
+		} else {
+			AlertDialog.Builder ad = new AlertDialog.Builder(this);
+			ad.setCancelable(false);
+			ad.setIcon(getResources().getDrawable(R.drawable.ic_warning_orange_24dp));
+			ad.setTitle(getString(R.string.request_already_sent));
+			ad.setMessage(getString(R.string.overwrite_pending_request));
+			ad.setPositiveButton(getResources().getString(R.string.yes), new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+					request();
+				}
+			});
+			ad.setNegativeButton(getResources().getString(R.string.no), new DialogInterface.OnClickListener() {
+				@Override
+				public void onClick(DialogInterface dialog, int which) {
+					dialog.dismiss();
+				}
+			});
+			ad.show();
+		}
+	}
+
+	private void request() {
+		buttonRequest.setEnabled(false);
+		buttonRequest.setText(getString(R.string.please_wait));
+
 		CharSequence sURI = editTextUrl.getText();
 		int isKeyLen = Integer.parseInt(String.valueOf(spinnerKeyLen.getSelectedItem()));
 
@@ -108,40 +148,74 @@ public class MainActivity extends AppCompatActivity {
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 		StrictMode.setThreadPolicy(policy);
 
-		try {
-			byte[] keystore = ScepClient.CertReq(
-					sURI.toString(),
-					editTextCommonName.getText().toString(),
-					editTextEnrollmentChallenge.getText().toString(),
-					editTextEnrollmentProfile.getText().toString(),
-					isKeyLen,
-					keystoreAlias,
-					editTextKeystorePassword.getText().toString()
-			);
+		Activity a = this;
+		AsyncTask.execute(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					byte[] keystore = ScepClient.CertReq(
+							sURI.toString(),
+							editTextCommonName.getText().toString(),
+							editTextEnrollmentChallenge.getText().toString(),
+							editTextEnrollmentProfile.getText().toString(),
+							isKeyLen,
+							keystoreAlias,
+							editTextKeystorePassword.getText().toString()
+					);
 
-			// import into system/user keystore
-			Intent intent = KeyChain.createInstallIntent();
-			intent.putExtra(KeyChain.EXTRA_PKCS12, keystore);
-			startActivity(intent);
+					// import into system/user keystore
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							Intent intent = KeyChain.createInstallIntent();
+							intent.putExtra(KeyChain.EXTRA_PKCS12, keystore);
+							startActivity(intent);
+						}
+					});
 
-		} catch(ScepClient.RequestPendingException e) {
-			CommonDialog.show(this, e.getClass().getName(), getString(R.string.request_pending_help), CommonDialog.Icon.WARN, false);
-			editTextTransactionId.setText(e.getTransactionId());
+				} catch(ScepClient.RequestPendingException e) {
+					SharedPreferences.Editor editor = sharedPref.edit();
+					editor.putString("cert", e.getCertPem());
+					editor.putString("key", e.getKeyPem());
+					editor.putString("tid", e.getTransactionId());
+					editor.apply();
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							CommonDialog.show(a, e.getClass().getName(), getString(R.string.request_pending_help), CommonDialog.Icon.WARN, false);
+							editTextTransactionId.setText(e.getTransactionId());
+						}
+					});
 
-			SharedPreferences.Editor editor = sharedPref.edit();
-			editor.putString("cert", e.getCertPem());
-			editor.putString("key", e.getKeyPem());
-			editor.putString("tid", e.getTransactionId());
-			editor.apply();
+				} catch(ScepClient.BadRequestException e) {
+					e.printStackTrace();
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							CommonDialog.show(a, e.getClass().getName(), getString(R.string.bad_request_help), CommonDialog.Icon.ERROR, false);
+						}
+					});
 
-		} catch(ScepClient.BadRequestException e) {
-			CommonDialog.show(this, e.getClass().getName(), getString(R.string.bad_request_help), CommonDialog.Icon.ERROR, false);
-			e.printStackTrace();
+				} catch(Exception e) {
+					e.printStackTrace();
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							CommonDialog.show(a, e.getClass().getName(), e.getMessage(), CommonDialog.Icon.ERROR, false);
+						}
+					});
 
-		} catch(Exception e) {
-			CommonDialog.show(this, e.getClass().getName(), e.getMessage(), CommonDialog.Icon.ERROR, false);
-			e.printStackTrace();
-		}
+				} finally {
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							buttonRequest.setEnabled(true);
+							buttonRequest.setText(getString(R.string.request));
+						}
+					});
+				}
+			}
+		});
 	}
 
 	public void onClickPoll(View view) {
@@ -152,54 +226,93 @@ public class MainActivity extends AppCompatActivity {
 			return;
 		}
 
+		buttonPoll.setEnabled(false);
+		buttonPoll.setText(getString(R.string.please_wait));
+
 		CharSequence sURI = editTextUrl.getText();
 
 		// enable some policies
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 		StrictMode.setThreadPolicy(policy);
 
-		try {
-			byte[] keystore = ScepClient.CertPoll(
-					sURI.toString(),
-					sharedPref.getString("cert", ""),
-					sharedPref.getString("key", ""),
-					editTextCommonName.getText().toString(),
-					sharedPref.getString("tid", ""),
-					keystoreAlias,
-					editTextKeystorePassword.getText().toString()
-			);
+		Activity a = this;
+		AsyncTask.execute(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					byte[] keystore = ScepClient.CertPoll(
+							sURI.toString(),
+							sharedPref.getString("cert", ""),
+							sharedPref.getString("key", ""),
+							editTextCommonName.getText().toString(),
+							sharedPref.getString("tid", ""),
+							keystoreAlias,
+							editTextKeystorePassword.getText().toString()
+					);
 
-			// import into system/user keystore
-			Intent intent = KeyChain.createInstallIntent();
-			intent.putExtra(KeyChain.EXTRA_PKCS12, keystore);
-			startActivity(intent);
+					// clear temp data
+					SharedPreferences.Editor editor = sharedPref.edit();
+					editor.remove("cert");
+					editor.remove("key");
+					editor.remove("tid");
+					editor.apply();
 
-			// clear temp data
-			SharedPreferences.Editor editor = sharedPref.edit();
-			editor.remove("cert");
-			editor.remove("key");
-			editor.remove("tid");
-			editor.apply();
-			editTextTransactionId.setText("");
+					// import into system/user keystore
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							Intent intent = KeyChain.createInstallIntent();
+							intent.putExtra(KeyChain.EXTRA_PKCS12, keystore);
+							startActivity(intent);
 
-		} catch(ScepClient.RequestPendingException e) {
-			CommonDialog.show(this, e.getClass().getName(), getString(R.string.request_pending_help), CommonDialog.Icon.WARN, false);
-			editTextTransactionId.setText(e.getTransactionId());
+							// clear temp data
+							editTextTransactionId.setText("");
+						}
+					});
 
-			SharedPreferences.Editor editor = sharedPref.edit();
-			editor.putString("cert", e.getCertPem());
-			editor.putString("key", e.getKeyPem());
-			editor.putString("tid", e.getTransactionId());
-			editor.apply();
+				} catch(ScepClient.RequestPendingException e) {
+					SharedPreferences.Editor editor = sharedPref.edit();
+					editor.putString("cert", e.getCertPem());
+					editor.putString("key", e.getKeyPem());
+					editor.putString("tid", e.getTransactionId());
+					editor.apply();
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							CommonDialog.show(a, e.getClass().getName(), getString(R.string.request_pending_help), CommonDialog.Icon.WARN, false);
+							editTextTransactionId.setText(e.getTransactionId());
+						}
+					});
 
-		} catch(ScepClient.BadRequestException e) {
-			CommonDialog.show(this, e.getClass().getName(), getString(R.string.bad_request_help), CommonDialog.Icon.ERROR, false);
-			e.printStackTrace();
+				} catch(ScepClient.BadRequestException e) {
+					e.printStackTrace();
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							CommonDialog.show(a, e.getClass().getName(), getString(R.string.bad_request_help), CommonDialog.Icon.ERROR, false);
+						}
+					});
 
-		} catch(Exception e) {
-			CommonDialog.show(this, e.getClass().getName(), e.getMessage(), CommonDialog.Icon.ERROR, false);
-			e.printStackTrace();
-		}
+				} catch(Exception e) {
+					e.printStackTrace();
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							CommonDialog.show(a, e.getClass().getName(), e.getMessage(), CommonDialog.Icon.ERROR, false);
+						}
+					});
+
+				} finally {
+					runOnUiThread(new Runnable() {
+						@Override
+						public void run() {
+							buttonPoll.setEnabled(true);
+							buttonPoll.setText(getString(R.string.poll));
+						}
+					});
+				}
+			}
+		});
 	}
 
 }
